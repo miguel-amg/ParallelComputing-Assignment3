97a1b26477f344a84973caf584040de23e9910bb
Migs082 <93313213+Migs082@users.noreply.github.com>
2024-11-18 14:55:51 +0000

    Update nos comandos só

--- comandos/antigos/perf_40c.sh
+++ comandos/antigos/perf_40c.sh
@@ -5,7 +5,7 @@ module load gcc/11.2.0
 echo ------------ Declaradas 40 threads ------------
 export OMP_NUM_THREADS=40
 echo ------------ A compilar programa ------------ 
-make
+make fluid_sim_par
 echo ------------ A enviar programa ------------ 
 echo A executar...
 srun --partition=cpar --ntasks=1 --cpus-per-task=40 --exclusive perf stat ./fluid_sim
