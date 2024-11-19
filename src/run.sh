1503feec3bb9a717cab9d0ccd16479165317c89a
Miguel <migueljacintocontact@gmail.com>
2024-11-19 16:02:42 +0000

    Final

--- src/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-#SBATCH --exclusive
-#SBATCH --time=02:00
-
-export OMP_NUM_THREADS=1
-perf stat -e cycles,instructions,branch-misses,L1-dcache-loads,L1-dcache-load-misses,mem-loads,mem-stores ./fluid_sim
-echo
